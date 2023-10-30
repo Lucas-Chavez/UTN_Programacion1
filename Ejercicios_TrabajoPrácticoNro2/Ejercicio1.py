144549f89fee7a6bc1c8986ee148813b4735c8b2
Lucas Chavez <138530379+Lucas-Chavez@users.noreply.github.com>
2023-10-30 19:53:36 -0300

    Delete Ejercicios_TrabajoPrácticoNro2 directory

--- Ejercicios_TrabajoPrácticoNro2/Ejercicio1.py
+++
@@ -1,9 +0,0 @@
-# Solicitar al usuario que ingrese el número de años de su computadora.
-anios_computadora = int(input("Ingrese el número de años que tiene su computadora: "))
-
-# Comprobar si la computadora tiene 2 años o menos.
-if anios_computadora <= 2:
-    print("Su computadora se considera nueva.")
-else:
-    # Si la computadora tiene más de 2 años, se considera vieja.
-    print("Su computadora se considera vieja.")
